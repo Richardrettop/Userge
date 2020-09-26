b9c6e3c536244de2149a5f6536a83cd3ae7c34bf
XMTS.CF <60861876+xmtscf@users.noreply.github.com>
2020-09-26 09:25:16 -0300

    Tradução afk.py
    Iniciando tradução

--- userge/plugins/utils/afk.py
+++ userge/plugins/utils/afk.py
@@ -70,8 +70,8 @@ async def handle_afk_incomming(message: Message) -> None:
     if user_id in USERS:
         if not (USERS[user_id][0] + USERS[user_id][1]) % randint(2, 4):
             if REASON:
-                out_str = (f"I'm still **AFK**.\nReason: <code>{REASON}</code>\n"
-                           f"Last Seen: `{afk_time} ago`")
+                out_str = (f"Ainda estou **AFK**.\nMotivo: <code>{REASON}</code>\n"
+                           f"Visto pela última vez: `{afk_time} atrás`")
             else:
                 out_str = choice(AFK_REASONS)
             coro_list.append(message.reply(out_str))
@@ -81,8 +81,8 @@ async def handle_afk_incomming(message: Message) -> None:
             USERS[user_id][1] += 1
     else:
         if REASON:
-            out_str = (f"I'm **AFK** right now.\nReason: <code>{REASON}</code>\n"
-                       f"Last Seen: `{afk_time} ago`")
+            out_str = (f"Estou **AFK** agora.\nMotivo: <code>{REASON}</code>\n"
+                       f"Visto pela última vez: `{afk_time} atrás`")
         else:
             out_str = choice(AFK_REASONS)
         coro_list.append(message.reply(out_str))
@@ -151,12 +151,12 @@ async def handle_afk_outgoing(message: Message) -> None:
 
 
 AFK_REASONS = (
-    "I'm busy right now. Please talk in a bag and when I come back you can just give me the bag!",
-    "I'm away right now. If you need anything, leave a message after the beep: \
+    "Agora estou ocupado. Por favor, fale em uma bolsa e quando eu voltar você pode apenas me dar a bolsa!",
+    "Estou fora agora. Se precisar de alguma coisa, deixe mensagem após o bip: \
 `beeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeep!`",
-    "You missed me, next time aim better.",
-    "I'll be back in a few minutes and if I'm not...,\nwait longer.",
-    "I'm not here right now, so I'm probably somewhere else.",
+    "Você sentiu minha falta, da próxima vez mire melhor.",
+    "Volto em alguns minutos e se não ...,\nespere mais.",
+    "Não estou aqui agora, então provavelmente estou em outro lugar.",
     "Roses are red,\nViolets are blue,\nLeave me a message,\nAnd I'll get back to you.",
     "Sometimes the best things in life are worth waiting for…\nI'll be right back.",
     "I'll be right back,\nbut if I'm not right back,\nI'll be back later.",
