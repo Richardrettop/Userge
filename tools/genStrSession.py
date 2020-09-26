f8a87ce09058a24408885dd5e0173315a1df1955
XMTS.CF <60861876+xmtscf@users.noreply.github.com>
2020-09-26 09:08:15 -0300

    fix

--- tools/genStrSession.py
+++ tools/genStrSession.py
@@ -1,8 +1,8 @@
 # pylint: disable=invalid-name, missing-module-docstring
 #
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/xmtscfTeam >.
+# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/xmtscf >.
 #
-# This file is part of < https://github.com/xmtscfTeam/Userge > project,
+# This file is part of < https://github.com/xmtscf/Userge > project,
 # and is released under the "GNU v3.0 License Agreement".
 # Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
 #
