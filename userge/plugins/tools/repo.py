8664e763a343bac222ddb809383c9961639dfc01
XMTS.CF <60861876+xmtscf@users.noreply.github.com>
2020-09-27 17:25:05 -0300

    Update repo.py

--- userge/plugins/tools/repo.py
+++ userge/plugins/tools/repo.py
@@ -9,17 +9,17 @@
 from userge import userge, Message, Config, versions, get_version
 
 
-@userge.on_cmd("repo", about={'header': "get repo link and details"})
+@userge.on_cmd("repo", about={'header': "obter link de repo e detalhes"})
 async def see_repo(message: Message):
     """see repo"""
     output = f"""
-**Hey**, __I am using__ 🔥 **Userge** 🔥
+**Oi**, __estou usando__ 🔥 **Userge do @xmtscf** 🔥
 
     __Durable as a Serge__
 
-• **userge version** : `{get_version()}`
+• **Versão do userge** : `{get_version()}`
 • **license** : {versions.__license__}
 • **copyright** : {versions.__copyright__}
-• **repo** : [Userge]({Config.UPSTREAM_REPO})
+• **repo** : [xmtscf]({Config.UPSTREAM_REPO})
 """
     await message.edit(output)
